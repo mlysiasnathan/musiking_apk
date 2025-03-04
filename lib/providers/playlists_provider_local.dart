0f2d36ff1b288c720973b684b2c57fa541cbc34d
NatCoding-devTeam <mlysiasnathan@gmail.com>
2025-03-04 22:29:47 +0200

    task : updating musiking UI and well organized workflow

--- lib/providers/playlists_provider_local.dart
+++ lib/providers/playlists_provider_local.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:on_audio_query/on_audio_query.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Playlists with ChangeNotifier {
-  final OnAudioQuery _audioQuery = OnAudioQuery();
   List<AlbumModel> playlists = [];
   bool isViewMoreAlbum = false;
+
   void viewMoreAlbum() {
     isViewMoreAlbum = !isViewMoreAlbum;
     notifyListeners();
@@ -17,24 +20,47 @@ class Playlists with ChangeNotifier {
       playlists.clear();
     }
     notifyListeners();
+    saveAlbumList();
   }
 
-  Future<List<AlbumModel>> setAlbumsList() async {
-    await _audioQuery
-        .queryAlbums(
-            sortType: null,
-            ignoreCase: true,
-            uriType: UriType.EXTERNAL,
-            orderType: OrderType.ASC_OR_SMALLER)
-        .then((albumsList) {
-      if (albumsList.isNotEmpty) {
-        playlists.clear();
-        playlists = albumsList;
-        notifyListeners();
-      } else {
-        playlists.clear();
-      }
-    });
-    return playlists;
+  void saveAlbumList() async {
+    final pref = await SharedPreferences.getInstance();
+    final albumListString =
+        playlists.map((playlist) => jsonEncode(playlist.getMap)).toList();
+    pref.setStringList('albums', albumListString);
   }
+
+  Future<void> loadPlaylists() async {
+    final pref = await SharedPreferences.getInstance();
+    final List<String> albumListString = pref.getStringList('albums') ?? [];
+    if (albumListString.isNotEmpty) {
+      playlists = albumListString
+          .map((songString) => AlbumModel(jsonDecode(songString)))
+          .toList();
+    }
+  }
+
+  void refreshPlaylists() {
+    playlists.clear();
+    notifyListeners();
+  }
+
+// Future<List<AlbumModel>> setAlbumsList() async {
+//   await _audioQuery
+//       .queryAlbums(
+//           sortType: null,
+//           ignoreCase: true,
+//           uriType: UriType.EXTERNAL,
+//           orderType: OrderType.ASC_OR_SMALLER)
+//       .then((albumsList) {
+//     if (albumsList.isNotEmpty) {
+//       playlists.clear();
+//       playlists = albumsList;
+//       notifyListeners();
+//     } else {
+//       playlists.clear();
+//     }
+//   });
+//   return playlists;
+// }
 }
