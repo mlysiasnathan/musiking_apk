ce7c4389f8988cd95f5a13970d99041eaa9b8a3f
NatCoding-devTeam <mlysiasnathan@gmail.com>
2025-03-04 22:32:09 +0200

    task : updating musiking UI and well organized workflow

--- lib/routes/refresh_screen.dart
+++ lib/routes/refresh_screen.dart
@@ -4,72 +4,73 @@ import 'package:provider/provider.dart';
 
 import '../providers/playlists_provider_local.dart';
 import '../providers/songs_provider.dart';
+import '../widgets/song/music_floating.dart';
 import '../widgets/song/song_card_one.dart';
 
 class RefreshScreen extends StatelessWidget {
   const RefreshScreen({super.key});
+
   static const routeName = '/refresh-screen';
 
   @override
   Widget build(BuildContext context) {
     final OnAudioQuery audioQuery = OnAudioQuery();
     final ThemeData theme = Theme.of(context);
-    final songData = Provider.of<Songs>(context, listen: false);
+    final songData = Provider.of<Songs>(context);
     final deviceSize = MediaQuery.of(context).size;
-    final playlistData = Provider.of<Playlists>(context, listen: false);
-    return Scaffold(
-      backgroundColor: theme.colorScheme.secondary,
-      floatingActionButton: ElevatedButton.icon(
-        onPressed: () {},
-        label: const Text("Refresh"),
-        icon: const Icon(Icons.refresh_sharp),
-      ),
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 8.0),
-            child: SizedBox(
-              height: 10,
-              width: 10,
-              child: FutureBuilder<List<SongModel>>(
-                future: audioQuery.querySongs(
-                  sortType: null,
-                  ignoreCase: true,
-                  uriType: UriType.EXTERNAL,
-                  orderType: OrderType.ASC_OR_SMALLER,
-                ),
-                builder: (context, items) {
-                  if (items.data == null) {
-                    return const CircularProgressIndicator(color: Colors.white);
-                  }
-                  return const SizedBox();
-                },
-              ),
-            ),
-          )
-        ],
-        title: Text(
-          'Refresh songs list',
-          style: theme.textTheme.headlineSmall!.copyWith(color: Colors.white),
+    final playlistData = Provider.of<Playlists>(context);
+    void refresh() {
+      songData.refreshSongs();
+      playlistData.refreshPlaylists();
+    }
+
+    songData.audioPlayer.currentIndexStream.listen((index) {
+      index != null
+          ? songData.saveCurrentSong(songData.currentPlaylist[index])
+          : null;
+    });
+
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [theme.colorScheme.primary, theme.colorScheme.secondary],
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20),
-              child: Row(
+      child: Scaffold(
+        floatingActionButton: const MusicFloating(),
+        floatingActionButtonLocation:
+            FloatingActionButtonLocation.miniCenterFloat,
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Text(
+            'All Songs',
+            style: theme.textTheme.headlineSmall!.copyWith(
+              color: theme.colorScheme.background,
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: refresh,
+              icon: const Icon(Icons.refresh),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   Container(
-                    height: 50,
+                    height: 70,
                     width: deviceSize.width * 0.45,
                     margin: const EdgeInsets.all(3),
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 3, vertical: 3),
+                    padding: const EdgeInsets.all(5),
                     decoration: BoxDecoration(
                       color: theme.primaryColor,
                       borderRadius: BorderRadius.circular(10),
@@ -78,10 +79,10 @@ class RefreshScreen extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
+                          borderRadius: BorderRadius.circular(7),
                           child: Container(
-                            width: 50,
-                            height: 50,
+                            width: 67,
+                            height: 67,
                             color:
                                 theme.colorScheme.background.withOpacity(0.7),
                             child: Icon(
@@ -108,12 +109,14 @@ class RefreshScreen extends StatelessWidget {
                                   )
                                 : FutureBuilder<List<AlbumModel>>(
                                     future: audioQuery.queryAlbums(
-                                        sortType: null,
-                                        ignoreCase: true,
-                                        uriType: UriType.EXTERNAL,
-                                        orderType: OrderType.ASC_OR_SMALLER),
-                                    builder: (context, items) {
-                                      if (items.data == null) {
+                                      sortType: null,
+                                      ignoreCase: true,
+                                      uriType: UriType.EXTERNAL,
+                                      orderType: OrderType.ASC_OR_SMALLER,
+                                    ),
+                                    builder: (context, snapshot) {
+                                      if (snapshot.connectionState ==
+                                          ConnectionState.waiting) {
                                         return Text(
                                           "Loading...",
                                           maxLines: 1,
@@ -125,7 +128,7 @@ class RefreshScreen extends StatelessWidget {
                                           ),
                                         );
                                       }
-                                      if (items.data!.isEmpty) {
+                                      if (snapshot.data!.isEmpty) {
                                         return Text(
                                           "Not Found",
                                           maxLines: 1,
@@ -140,10 +143,10 @@ class RefreshScreen extends StatelessWidget {
                                       Future.delayed(
                                         Duration.zero,
                                         () => playlistData.getAlbumsList(
-                                            albumsList: items.data!),
+                                            albumsList: snapshot.data!),
                                       );
                                       return Text(
-                                        items.data!.length.toString(),
+                                        snapshot.data!.length.toString(),
                                         maxLines: 1,
                                         overflow: TextOverflow.ellipsis,
                                         style: theme.textTheme.titleMedium!
@@ -167,11 +170,10 @@ class RefreshScreen extends StatelessWidget {
                     ),
                   ),
                   Container(
-                    height: 50,
+                    height: 70,
                     width: deviceSize.width * 0.45,
                     margin: const EdgeInsets.all(3),
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 3, vertical: 3),
+                    padding: const EdgeInsets.all(5),
                     decoration: BoxDecoration(
                       color: theme.primaryColor,
                       borderRadius: BorderRadius.circular(10),
@@ -180,10 +182,10 @@ class RefreshScreen extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
+                          borderRadius: BorderRadius.circular(7),
                           child: Container(
-                            width: 50,
-                            height: 50,
+                            width: 67,
+                            height: 67,
                             color:
                                 theme.colorScheme.background.withOpacity(0.7),
                             child: Icon(
@@ -208,49 +210,16 @@ class RefreshScreen extends StatelessWidget {
                                       color: theme.colorScheme.background,
                                     ),
                                   )
-                                : FutureBuilder<List<SongModel>>(
-                                    future: audioQuery.querySongs(
-                                      sortType: null,
-                                      ignoreCase: true,
-                                      uriType: UriType.EXTERNAL,
-                                      orderType: OrderType.ASC_OR_SMALLER,
+                                : Text(
+                                    "Loading...",
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                    style:
+                                        theme.textTheme.titleMedium!.copyWith(
+                                      fontWeight: FontWeight.bold,
+                                      color: theme.colorScheme.background,
                                     ),
-                                    builder: (context, items) {
-                                      if (items.data == null) {
-                                        return Text(
-                                          "Loading...",
-                                          maxLines: 1,
-                                          overflow: TextOverflow.ellipsis,
-                                          style: theme.textTheme.titleMedium!
-                                              .copyWith(
-                                            fontWeight: FontWeight.bold,
-                                            color: theme.colorScheme.background,
-                                          ),
-                                        );
-                                      }
-                                      if (items.data!.isEmpty) {
-                                        return Text(
-                                          "Not Found",
-                                          maxLines: 1,
-                                          overflow: TextOverflow.ellipsis,
-                                          style: theme.textTheme.titleMedium!
-                                              .copyWith(
-                                            fontWeight: FontWeight.bold,
-                                            color: theme.colorScheme.background,
-                                          ),
-                                        );
-                                      }
-                                      return Text(
-                                        items.data!.length.toString(),
-                                        maxLines: 1,
-                                        overflow: TextOverflow.ellipsis,
-                                        style: theme.textTheme.titleMedium!
-                                            .copyWith(
-                                          fontWeight: FontWeight.bold,
-                                          color: theme.colorScheme.background,
-                                        ),
-                                      );
-                                    }),
+                                  ),
                             Text(
                               'songs',
                               maxLines: 1,
@@ -266,9 +235,8 @@ class RefreshScreen extends StatelessWidget {
                   ),
                 ],
               ),
-            ),
-            Consumer<Songs>(
-              builder: (ctx, songData, _) => Expanded(
+              const Divider(),
+              Expanded(
                 child: songData.songs.isEmpty
                     ? FutureBuilder<List<SongModel>>(
                         future: audioQuery.querySongs(
@@ -277,36 +245,19 @@ class RefreshScreen extends StatelessWidget {
                           uriType: UriType.EXTERNAL,
                           orderType: OrderType.ASC_OR_SMALLER,
                         ),
-                        builder: (context, items) {
-                          if (items.data == null) {
+                        builder: (context, snapshots) {
+                          if (snapshots.connectionState ==
+                              ConnectionState.waiting) {
                             return ListView.builder(
-                              shrinkWrap: true,
-                              addRepaintBoundaries: true,
-                              addAutomaticKeepAlives: true,
-                              padding: const EdgeInsets.only(top: 4),
-                              physics: const NeverScrollableScrollPhysics(),
                               itemCount: 10,
-                              itemBuilder: (context, index) {
-                                return Container(
-                                  height: 50,
-                                  margin: const EdgeInsets.all(3),
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 3, vertical: 3),
-                                  decoration: BoxDecoration(
-                                    border: Border.all(
-                                      width: 2,
-                                      color: Colors.white,
-                                    ),
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                );
-                              },
+                              itemBuilder: (context, index) =>
+                                  const SongCardLoading(),
                             );
                           }
-                          if (items.data!.isEmpty) {
+                          if (snapshots.data!.isEmpty) {
                             return Center(
                               child: Text(
-                                "Not Found",
+                                "Songs Not Found",
                                 maxLines: 1,
                                 overflow: TextOverflow.ellipsis,
                                 style: theme.textTheme.titleLarge!.copyWith(
@@ -318,17 +269,20 @@ class RefreshScreen extends StatelessWidget {
                           }
                           Future.delayed(
                             Duration.zero,
-                            () => songData.getSongsList(songsList: items.data!),
+                            () => songData.getSongsList(
+                              songsList: snapshots.data!,
+                            ),
                           );
                           return ListView.builder(
                             shrinkWrap: true,
                             addRepaintBoundaries: true,
                             addAutomaticKeepAlives: true,
-                            itemCount: items.data!.length,
+                            itemCount: snapshots.data!.length,
                             itemBuilder: (context, index) => SongCardOne(
+                              playLists: snapshots.data!,
                               key: ValueKey(index),
-                              song: items.data![index],
-                              index: items.data![index].id,
+                              song: snapshots.data![index],
+                              index: index,
                             ),
                           );
                         },
@@ -339,14 +293,15 @@ class RefreshScreen extends StatelessWidget {
                         addAutomaticKeepAlives: true,
                         itemCount: songData.songs.length,
                         itemBuilder: (context, index) => SongCardOne(
+                          playLists: songData.songs,
                           key: ValueKey(index),
                           song: songData.songs[index],
-                          index: songData.songs[index].id,
+                          index: index,
                         ),
                       ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
